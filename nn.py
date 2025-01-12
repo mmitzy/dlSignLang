e609715b03368c2666ba33f9cec6e3f33421c234
Lidor Borofker <155881749+Lidx7@users.noreply.github.com>
2025-01-12 19:52:44 +0200

    Update nn.py

--- nn.py
+++ nn.py
@@ -6,6 +6,7 @@ from torchvision import datasets, transforms
 import os
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import Evaluate as EV
 
 
 class SimpleNeuralNetwork(nn.Module):
@@ -68,33 +69,39 @@ class SimpleNeuralNetwork(nn.Module):
         self.to(device)
         print(f"Model loaded from {file_path}")
 
-    def evaluate_model(self, test_loader, device):
-        self.to(device)  # Move model to device
-        self.eval()  # Set model to evaluation mode
-        correct = 0
-        total = 0
-        all_labels = []
-        all_predicitions = []
+    # def evaluate_model(self, test_loader, device):
+    #     self.to(device)  # Move model to device
+    #     self.eval()  # Set model to evaluation mode
+    #     correct = 0
+    #     total = 0
+    #     all_labels = []
+    #     all_predicitions = []
         
-        with torch.no_grad():  # Disable gradient computation
-            for images, labels in test_loader:
-                images, labels = images.to(device), labels.to(device)
-                outputs = self(images)
-                _, predicted = torch.max(outputs, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+    #     with torch.no_grad():  # Disable gradient computation
+    #         for images, labels in test_loader:
+    #             images, labels = images.to(device), labels.to(device)
+    #             outputs = self(images)
+    #             _, predicted = torch.max(outputs, 1)
+    #             total += labels.size(0)
+    #             correct += (predicted == labels).sum().item()
 
-                all_labels.extend(labels.cpu().numpy())
-                all_predicitions.extend(predicted.cpu().numpy())
+    #             all_labels.extend(labels.cpu().numpy())
+    #             all_predicitions.extend(predicted.cpu().numpy())
         
-        accuracy = 100 * correct / total
-        print(f'Accuracy of the model on the test set: {accuracy:.2f}%')
+    #     accuracy = 100 * correct / total
+    #     print(f'Accuracy of the model on the test set: {accuracy:.2f}%')
 
-        cm = confusion_matrix(all_labels, all_predicitions)
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=range(len(cm)))
-        disp.plot(cmap=plt.cm.Blues)
-        plt.title("Confusion Matrix(Results)")
-        plt.show()
+    #     cm = confusion_matrix(all_labels, all_predicitions)
+    #     plt.figure(figsize=(15,12))
+    #     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=range(len(cm)))
+    #     disp.plot(cmap=plt.cm.Blues, xticks_rotation=285)
+    #     plt.xlabel('Predicted label', labelpad=20)
+    #     plt.title("Confusion Matrix(Results)")
+    #     plt.tight_layout()
+    #     plt.savefig("confusion_matrix_nn.png", dpi=300)
+    #     plt.show()
+    #     print("Confusion matrix saved as 'confusion_matrix.png'.")
+
 
 # -------------------------------
 # Example Usage
@@ -146,4 +153,4 @@ else:
 
 # Evaluate the model
 print("Evaluating the model...")
-model.evaluate_model(test_loader, device)
+model.EV.evaluate_model(test_loader, device)
